9999ece866a33c31bd10b6a9db2abf5c9ff444aa
yh725 <yh725@kakao.com>
2022-11-26 22:25:02 +0900

    영속성 관리(내부 동작 방식) - 준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,10 +17,14 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 200L);
+            member.setName("AAA");
 
-            em.flush();
+//            em.detach(member);
+            em.clear();
+//            em.close();
+
+            Member member2 = em.find(Member.class, 200L);
 
             System.out.println("===========");
 
