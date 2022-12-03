ee77f6cb430e757c5d4b5d81f8d13e537d7624e8
yh725 <yh725@kakao.com>
2022-12-03 22:44:27 +0900

    다양한 연관관계 매핑 - 다대일 [N:1]

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
-/*public class JpaMain {
+public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -18,31 +18,31 @@ import java.util.List;
 
         try {
 
-            *//*Team team = new Team();
+            Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeamId(team.getId());
+//            member.setTeamId(team.getId());
             em.persist(member);
 
             Member findMember = em.find(Member.class, member.getId());
 
-            Long findTeamId = findMember.getTeamId();
-            Team findTeam = em.find(Team.class, findTeamId);*//*
+//            Long findTeamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, findTeamId);
 
-            Team team = new Team();
-            team.setName("TeamA");
+//            Team team = new Team();
+//            team.setName("TeamA");
 //            team.getMemberList().add(member);
             em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("member1");
+//            Member member = new Member();
+//            member.setUsername("member1");
 //            member.changeTeam(team);   //**
             em.persist(member);
 
-            team.addMember(member);
+//            team.addMember(member);
 
             //역방향(주인이 아닌 방향)만 연관관계 설정
 //            team.getMemberList().add(member);   //양쪽으로 세팅해주는게 맞음.   //** 연관관계 편의 메소드
@@ -51,11 +51,11 @@ import java.util.List;
 //            em.clear();
 
             Team findTeam = em.find(Team.class, team.getId());  //1차 캐시
-            List<Member> members = findTeam.getMembers();
+//            List<Member> members = findTeam.getMembers();
 
-            for (Member m : members) {
-                System.out.println("m = " + m.getUsername());
-            }
+//            for (Member m : members) {
+//                System.out.println("m = " + m.getUsername());
+//            }
 
             tx.commit();
         } catch (Exception e) {
@@ -67,4 +67,4 @@ import java.util.List;
         emf.close();
 
     }
-}*/
+}
--- src/main/java/jpabook/jpashop/domain/JpaMain.java
+++ src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+/*
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -36,3 +37,4 @@ public class JpaMain {
 
     }
 }
+*/
