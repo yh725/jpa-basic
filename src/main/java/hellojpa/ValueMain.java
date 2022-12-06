eda87d506e760bdc7d636ad9669fbf53a541f0c4
yh725 <yh725@kakao.com>
2022-12-06 20:35:16 +0900

    값 타입 - 값 타입의 비교

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -11,6 +11,7 @@ public class ValueMain {
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+        System.out.println("a == b:" + (a == b));
 
         Integer c = new Integer(10);
         Integer d = c;
@@ -19,5 +20,10 @@ public class ValueMain {
 
         System.out.println("c = " + c);
         System.out.println("d = " + d);
+
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("address1 == address2:" + (address1 == address2));
+        System.out.println("address1 equals address2:" + (address1.equals(address2)));
     }
 }
