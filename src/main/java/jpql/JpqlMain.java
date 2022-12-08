8a2a5bcdba406a1403dc4e7c3be019b8111e25d2
yh725 <yh725@kakao.com>
2022-12-08 19:23:56 +0900

    객체지향 쿼리 언어2(중급 문법) - 경로 표현식

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpqlMain {
@@ -15,42 +16,40 @@ public class JpqlMain {
 
 		try {
 
-			/*Team team = new Team();
+			Team team = new Team();
 			team.setName("TeamA");
-			em.persist(team);*/
+			em.persist(team);
 
 			Member member = new Member();
 			member.setUsername("관리자");
 			member.setAge(10);
 			member.setType(MemberType.ADMIN);
-//			member.setTeam(team);
+			member.setTeam(team);
 			em.persist(member);
 
 			Member member2 = new Member();
 			member2.setUsername("관리자2");
 			member2.setAge(10);
 			member2.setType(MemberType.ADMIN);
-//			member2.setTeam(team);
+			member2.setTeam(team);
 			em.persist(member2);
 
 			em.flush();
 			em.clear();
 
-//			String query = "select 'a' || 'b' from MemberJ m";
-//			String query = "select concat('a', 'b') from MemberJ m";
-//			String query = "select substring(m.username, 2, 3) from MemberJ m";
-//			String query = "select locate('de', 'abcdef') from MemberJ m";
-//			String query = "select size(t.members) from TeamJ t";
-			/*@OrderColumn
-			String query = "select index(t.members) from TeamJ t";*/
-//			String query = "select function('group_concat', m.username) from MemberJ m";
-			String query = "select group_concat(m.username) from MemberJ m";
-			List<String> result = em.createQuery(query, String.class)
+//			String query = "select m.username from MemberJ m"; //상태 필드
+//			String query = "select m.team from MemberJ m"; //단일 값 연관 경로(묵시적인 내부 조인), 탐색 o - 웬만해선 배제
+//			String query = "select t.members from TeamJ t"; //컬렉션 값 연관 경로, 탐색 x
+			String query = "select m from TeamJ t join t.members m"; //명시적 조인
+			List<Collection> result = em.createQuery(query, Collection.class)
 					.getResultList();
 
-			for (String s : result) {
-				System.out.println("s = " + s);
+			for (Object o : result) {
+				System.out.println("o = " + o);
 			}
+			/*for (Member s : result) {
+				System.out.println("s = " + s);
+			}*/
 
 			tx.commit();
 		} catch (Exception e) {
